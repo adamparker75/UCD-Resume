c522b111d0b0d87a9b054235625dc3296b6bf657
adamparker75 <adamparker75@sky.com>
2020-06-09 18:48:50 +0000

    Fix bugs for the gh-data and gh-repo elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,7 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-
+    $("#gh-usuer-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -70,3 +71,5 @@ function fetchGitHubInformation(event) {
             }
         });
 }
+
+document.readyState(fetchGitHubInformation);
